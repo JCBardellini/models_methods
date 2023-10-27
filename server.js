26032887b6f4e027cf34d0a042685193bd70856c
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2023-10-27 13:23:15 -0700

    finish lab

--- server.js
+++ server.js
@@ -24,9 +24,38 @@ app.use(helmet());
 
 
 // find   - finds everything
+
 // .find()
  
+app.get("/books", async (req, res) => {
+    let response = await Book.find()
+    res.send(response)
+})
+
+// findOne
+
+app.get("/books/title/:title", async (req, res) => {
+    let title = req.params.title
+    let response = await Book.findOne({ title: title})
+    console.log(response);
+    res.send(response)
+})
+
 // findById
+
+app.get("/books/id/:bookId", async (req, res) => {
+    let bookId = req.params.bookId
+    let response = await Book.findById(bookId)
+    res.send(response)
+})
+
+// create
+app.post("/books/singleBook", async (req, res) => {
+    let singleBook = req.body.singleBook
+
+    let response = await Book.create(singleBook)
+    res.send(response)
+})
 
 // insertMany
 app.post('/books', async (req, res) => {
@@ -37,8 +66,44 @@ app.post('/books', async (req, res) => {
     res.send(dbResponse);
 })
 
-// findOne
+// deleteOne
 
+
+// DeleteMany
+app.delete("/books", async (req, res) => {
+    let response = await Book.deleteMany({pages: {$gte: 200}} )
+    res.send('Books Deleted')
+})
+
+// app.delete("/books", async (req, res) => {
+//     try {
+//         const response = await Book.deleteMany({ pages: { $gt: 250 } });
+//         res.send('Books Deleted');
+//     } catch (err) {
+//         res.status(500).send({ error: "Internal server error" });
+//     }
+// });
+
+
+app.delete("/books/:title", async (req, res) => {
+    let title = req.params.title
+    let response = await Book.deleteOne({title : title});
+    res.send("book deleted")
+})
+
+app.delete("/books/id/:bookId", async (req, res) => {
+    let bookId = req.params.bookId
+    let response = await Book.findByIdAndDelete(bookId)
+    res.send("book deleted")
+})
+
+
+app.put("/books/updatedBook", async (req, res) => {
+    let updatedBook = req.body.updatedBook
+    let response = await Book.updateOne({title : updatedBook.title}, updatedBook)
+    res.send(response)
+
+})
 // END ROUTES //
 
 app.listen(PORT, () => {
